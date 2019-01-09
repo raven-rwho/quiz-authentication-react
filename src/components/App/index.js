d5f70b588aef232f8a3d81e3a87ec3a31268286d
Peter Krauss <peter.krauss@upday.com>
2019-01-09 19:19:38 +0100

    refactor questionlist to higher order component - but still with fetching the user table

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,7 +9,7 @@ import PasswordForgetPage from '../PasswordForget';
 import HomePage from '../Home';
 import AccountPage from '../Account';
 import AdminPage from '../Admin';
-import QuestionListPage from '../Questions'
+import QuestionListPage from '../QuestionList'
 
 import * as ROUTES from '../../constants/routes';
 import { withAuthentication } from '../Session';
